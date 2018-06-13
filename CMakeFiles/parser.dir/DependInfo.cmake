b3f9ea2ca3efe333e133afc47b2bf7ed76bd0764
Mitchell <mlkeller@calpoly.edu>
2018-06-13 12:42:04 -0700

    Soft Shadows working but a little staticy

---
+++ CMakeFiles/parser.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mlkeller/cpe473/part5/lib/parser/Parser.cpp" "/home/mlkeller/cpe473/part5/build/CMakeFiles/parser.dir/lib/parser/Parser.cpp.o"
+  "/home/mlkeller/cpe473/part5/lib/parser/TokenStream.cpp" "/home/mlkeller/cpe473/part5/build/CMakeFiles/parser.dir/lib/parser/TokenStream.cpp.o"
+  "/home/mlkeller/cpe473/part5/lib/parser/Tokenizer.cpp" "/home/mlkeller/cpe473/part5/build/CMakeFiles/parser.dir/lib/parser/Tokenizer.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ CMakeFiles/raytrace.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/raytrace.dir/src/Application/Application.cpp.o"
+  "CMakeFiles/raytrace.dir/src/Application/main.cpp.o"
+  "CMakeFiles/raytrace.dir/src/Application/RayInfo.cpp.o"
+  "CMakeFiles/raytrace.dir/src/Application/Renderer.cpp.o"
+  "CMakeFiles/raytrace.dir/src/Application/SceneInfo.cpp.o"
+  "CMakeFiles/raytrace.dir/src/Objects/Box.cpp.o"
+  "CMakeFiles/raytrace.dir/src/Objects/Plane.cpp.o"
+  "CMakeFiles/raytrace.dir/src/Objects/Sphere.cpp.o"
+  "CMakeFiles/raytrace.dir/src/Objects/Triangle.cpp.o"
+  "CMakeFiles/raytrace.dir/src/RayTracer/Pixel.cpp.o"
+  "CMakeFiles/raytrace.dir/src/RayTracer/RayTracer.cpp.o"
+  "CMakeFiles/raytrace.dir/src/RayTracer/Util.cpp.o"
+  "CMakeFiles/raytrace.dir/src/Scene/AABB.cpp.o"
+  "CMakeFiles/raytrace.dir/src/Scene/BoundingVolumeNode.cpp.o"
+  "CMakeFiles/raytrace.dir/src/Scene/Camera.cpp.o"
+  "CMakeFiles/raytrace.dir/src/Scene/Object.cpp.o"
+  "CMakeFiles/raytrace.dir/src/Scene/Scene.cpp.o"
+  "CMakeFiles/raytrace.dir/src/Shading/BlinnPhongBRDF.cpp.o"
+  "CMakeFiles/raytrace.dir/src/Shading/CookTorranceBRDF.cpp.o"
+  "raytrace.pdb"
+  "raytrace"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/raytrace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
